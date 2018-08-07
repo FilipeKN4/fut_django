fabaa6d58c46a7bdc9c0ad59b16a5aac6bd6d819
FilipeKN4 <filipekn4@gmail.com>
2018-08-07 15:20:29 -0300

    Create model Time

--- liga/models.py
+++ liga/models.py
@@ -3,3 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Time(models.Model):
+    nome = models.CharField(max_length=100)
+    cidade = models.CharField(max_length=100, blank=True, null=True)
+    sigla_estado = models.CharField(max_length=2, blank=True, null=True)
+    numero_de_torcedores = models.IntegerField(blank=True, null=True)
+    numero_de_titulos = models.IntegerField(blank=True, null=True)
+
+    def __unicode__(self):
+        return self.nome

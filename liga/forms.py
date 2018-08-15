666923bc3b405084fd0c3500729f9b8fff0e0b84
FilipeKN4 <filipekn4@gmail.com>
2018-08-15 10:41:39 -0300

    Add templates changes

---
+++ liga/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+from liga.models import Time
+
+class TimeForm(forms.Form):
+    nome = forms.CharField()
+    #cidade = forms.CharField()
+    sigla_estado = forms.CharField()
+    #numero_de_torcedores = forms.IntegerField()
+    #numero_de_titulos = forms.IntegerField()
+
+class TimeModelForm(forms.ModelForm):
+    class Meta:
+        model = Time
+        fields = ["nome", "sigla_estado"]
+
+    def clean_sigla_estado(self):
+        sigla_estado = self.cleaned_data.get("sigla_estado")
+        if len(sigla_estado) > 2:
+            raise forms.ValidationError("Cara, somente siglas de estados! Vlw")
+        return sigla_estado
